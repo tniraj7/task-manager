e1e175a7fb2ed716305961346649fd497df6a57b
Niraj Tiwari <tniraj7@gmail.com>
2019-03-24 23:21:16 +0900

    * add patch method for users * add allowed modify operations on users

--- src/index.js
+++ src/index.js
@@ -75,6 +75,25 @@ app.get("/tasks/:id", async (req, res) => {
   }
 })
 
+app.patch('/users/:id', async(req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ["name", "email", "password", "age"]
+  const isValidOperation = updates.every( (update) =>  allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({'error': 'Invalid update!'})
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+    if (!user) {
+      res.status(404).send
+    }
+    res.send(user)
+  } catch(e) {
+    res.status(400).send(e)
+  }
+})
+
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
 })
