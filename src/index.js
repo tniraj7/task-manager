74692e79998cb3aac710d8e8437543df1038db29
Niraj Tiwari <tniraj7@gmail.com>
2019-03-25 20:00:03 +0900

    * added Delete API method over User model

--- src/index.js
+++ src/index.js
@@ -115,6 +115,20 @@ app.patch('/tasks/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+
+  try {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if (!user) {
+      res.status(404).send()
+    }
+    res.send(user)
+  } catch (e) {
+    res.status(500).send()
+  }
+
+})
+
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
 })
