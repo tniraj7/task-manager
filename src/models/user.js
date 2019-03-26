7db497fbf83e32c1d8090ee02d8bfa0244dcc324
Niraj Tiwari <tniraj7@gmail.com>
2019-03-26 22:44:43 +0900

    use middleware in asycn updae function for password hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcrypt')
 
-const User = mongoose.model('User', {
+const userSchema = mongoose.Schema({
     name: { type: String, require: true, trim: true },
     password: {
         type: String,
@@ -36,5 +37,13 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+})
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
