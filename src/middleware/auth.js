ec60afac9817af02311beea61ebc7ff51b854030
Niraj Tiwari <tniraj7@gmail.com>
2019-04-02 23:49:58 +0900

    fixed indentation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
-const auth = async (req, res , next) => {
+const auth = async (req, res, next) => {
    try {
 
     const token  = req.header('Authorization').replace('Bearer', '')
