ff6ebdc3e7f328a9819a1e65e8a893b153cf6b99
Niraj Tiwari <tniraj7@gmail.com>
2019-04-15 20:59:39 +0900

    modify the model definition for Task

--- src/models/task.js
+++ src/models/task.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const Task = mongoose.model('Task', {
     description: { type: String, required: true, trim: true },
-    completed: { type: Boolean, required: false, default: false }
+    completed: { type: Boolean, required: false, default: false },
+    ower: {type : mongoose.Schema.Types.ObjectId, required : true}
 })
 
 module.exports = Task
