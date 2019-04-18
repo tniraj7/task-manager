10ae56ef285e676552ad862a9bb2d01b2066ef09
Niraj Tiwari <tniraj7@gmail.com>
2019-04-18 23:47:42 +0900

    return tasks only for authenticated user

--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -19,7 +19,8 @@ router.post("/tasks", auth, async (req, res) => {
 
 router.get("/tasks", auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
+        // const tasks = await Task.find({owner: req.user._id})
+        await req.user.populate('tasks').execPopulate()
         res.status(201).send(tasks)
     } catch (e) {
         res.status(500).send()
