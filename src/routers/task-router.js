2b71061900a48f9fa6842ead293923d22288447d
Niraj Tiwari <tniraj7@gmail.com>
2019-04-18 23:44:15 +0900

    add auth to read tasks method

--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -17,7 +17,7 @@ router.post("/tasks", auth, async (req, res) => {
     }
 })
 
-router.get("/tasks", async (req, res) => {
+router.get("/tasks", auth, async (req, res) => {
     try {
         const tasks = await Task.find({})
         res.status(201).send(tasks)
@@ -31,7 +31,7 @@ router.get("/tasks/:id", auth, async (req, res) => {
 
     try {
 
-        const task = await Task.findOne({_id, owner: req.user._id})
+        const task = await Task.findOne({_id, owner: req.user._id })
         if (!task) {
             return res.status(404).send()
         }
