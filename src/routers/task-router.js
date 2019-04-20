b0f45abc172aa585323ed4c0b695e133a39e1a83
Niraj Tiwari <tniraj7@gmail.com>
2019-04-20 20:48:49 +0900

    Update the task for logged in user only

--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -44,7 +44,7 @@ router.get("/tasks/:id", auth, async (req, res) => {
 
 
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ["description", "completed"]
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -54,13 +54,15 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
-        updates.forEach((update) => task[update] = req.body[update])
-        await task.save()
+        const task = await Task.findOne({ _id : req.params.id, owner : req.user._id})
 
         if (!task) {
             res.status(404).send()
         }
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+
         res.send(task)
     } catch (e) {
         res.status(500).send(e)
